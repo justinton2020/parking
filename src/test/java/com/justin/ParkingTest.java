a6afde392970a8c9cfb9e6af47cd1b2244ea5e18
nguyenton2020@gmail.com <nguyenton2020@gmail.com>
2019-10-27 01:16:49 -0500

    make maven project

---
+++ src/test/java/com/justin/ParkingTest.java
@@ -0,0 +1,18 @@
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class ParkingTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
